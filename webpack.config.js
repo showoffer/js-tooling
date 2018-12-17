a6f5985fadd6d29d7c4e43212c2ed89b17353bec
Alex Dzeiko <aliaksei.dzeiko@pandadoc.com>
2018-12-17 18:17:12 +0300

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'app.bundle.js',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'src/index.html'
+  })]
+};
